2d196566069bf09a20b7794f8a780ad058da7697
jmoussalli <jerome.moussalli@gmail.com>
2024-01-26 11:47:52 +0100

    First commit

---
+++ src/main/java/com/example/demo/controller/PersonneController.java
@@ -0,0 +1,44 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Personne;
+import com.example.demo.service.Annuaire;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("api")
+public class PersonneController {
+
+    private Annuaire annuaire = new Annuaire();
+
+    // GET /personnes
+    @GetMapping("personnes")
+    public List<Personne> getPersonnes(){
+//        ArrayList<Personne> personnes = new ArrayList<>();
+//        personnes.add(new Personne("JC", "Dominguez"));
+//        personnes.add(new Personne("Marie", "Dupont"));
+        return annuaire.getAll();
+    }
+
+    // POST /personnes
+    @PostMapping("personnes")
+    public void addPersonne(@RequestBody Personne newPersonne){
+        System.out.println(newPersonne);
+        annuaire.add(newPersonne);
+    }
+
+    //GET /personnes/4
+    @GetMapping("personnes/{id}")
+    public Personne getById(@PathVariable Integer id){
+        return annuaire.findById(id);
+    }
+
+    //DELETE /personnes/4
+    @DeleteMapping("personnes/{id}")
+    public void delete(@PathVariable Integer id){
+        annuaire.delete(id);
+    }
+
+}
---
+++ src/main/java/com/example/demo/service/Annuaire.java
@@ -0,0 +1,30 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Personne;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class Annuaire {
+
+    private int idCount;
+    private HashMap<Integer, Personne> personnes = new HashMap<Integer, Personne>();
+
+    public void add(Personne p){
+        idCount++;
+        p.setId(idCount);
+        personnes.put(idCount, p);
+    }
+
+    public List<Personne> getAll(){
+        return personnes.values().stream().toList();
+    }
+
+    public Personne findById(Integer id){
+        return personnes.get(id);
+    }
+
+    public void delete(Integer id) {
+        personnes.remove(id);
+    }
+}
